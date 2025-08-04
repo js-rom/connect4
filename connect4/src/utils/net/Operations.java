4b3a9eba10da41daf2bf8f516e38f9dab9d379e9
[jesus romero] <jesus.romerovidal@gmail.com>
2025-08-04 17:49:20 +0200

    refactor: add interface Operations #7

---
+++ connect4/src/utils/net/Operations.java
@@ -0,0 +1,36 @@
+package utils.net;
+
+import java.io.IOException;
+import java.net.Socket;
+
+import connect4.types.Color;
+import connect4.types.PlayerType;
+
+public interface Operations {
+
+    public void associate(Socket socket) throws IOException;
+
+    public void send(String value);
+
+    public void send(Integer value);
+
+    public void send(Boolean value);
+
+    public void send(char value);
+
+    public void send(PlayerType playerType);
+
+    public void send(PlayerType[] playerTypes);
+
+    public void send(Color color);
+
+    public String receiveLine() throws IOException;
+
+    public boolean receiveBoolean() throws IOException;
+
+    public Integer receiveInt() throws IOException;
+
+    public char receiveChar() throws IOException;
+
+    public void close() throws IOException;
+}
