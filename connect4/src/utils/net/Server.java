b6facf0c1b06e8889b05bbd82938ef3c79db0da7
[jesus romero] <jesus.romerovidal@gmail.com>
2025-08-04 18:34:10 +0200

    refactor: Server implements Operations interface and delegates all it operations to CharactersTcpipSocket #7

--- connect4/src/utils/net/Server.java
+++ connect4/src/utils/net/Server.java
@@ -2,8 +2,12 @@ package utils.net;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
-public class Server extends CharactersTcpipSocket {
+import connect4.types.Color;
+import connect4.types.PlayerType;
+
+public class Server implements Operations {
 
     private Operations socket;
     private ServerSocket serverSocket;
@@ -25,6 +29,91 @@ public class Server extends CharactersTcpipSocket {
         System.out.println("Servidor> Recibida conexion de " + this.socket.getHostAddress() + ":"
                 + this.socket.getPort());
 
+    }
+
+    @Override
+    public void associate(Socket socket) throws IOException {
+        this.socket.associate(socket);
+    }
+
+    @Override
+    public void send(String value) {
+        this.socket.send(value);
+    }
+
+    @Override
+    public void send(Integer value) {
+        this.socket.send(value);
+    }
+
+    @Override
+    public void send(Boolean value) {
+        this.socket.send(value);
+    }
+
+    @Override
+    public void send(char value) {
+        this.socket.send(value);
+    }
+
+    @Override
+    public void send(PlayerType playerType) {
+        this.socket.send(playerType);
+    }
+
+    @Override
+    public void send(PlayerType[] playerTypes) {
+        this.socket.send(playerTypes);
+    }
+
+    @Override
+    public void send(Color color) {
+        this.socket.send(color);
+    }
+
+    @Override
+    public String receiveLine() throws IOException {
+        return this.socket.receiveLine();
+    }
+
+    @Override
+    public boolean receiveBoolean() throws IOException {
+        return this.socket.receiveBoolean();
+    }
+
+    @Override
+    public Integer receiveInt() throws IOException {
+        return this.socket.receiveInt();
+    }
+
+    @Override
+    public char receiveChar() throws IOException {
+        return this.socket.receiveChar();
+    }
+
+    @Override
+    public PlayerType receivePlayerType() throws IOException {
+        return this.socket.receivePlayerType();
+    }
+
+    @Override
+    public PlayerType[] receivePlayerTypes() throws IOException {
+        return this.socket.receivePlayerTypes();
+    }
+
+    @Override
+    public Color receiveColor() throws IOException {
+        return this.socket.receiveColor();
+    }
+
+    @Override
+    public String getHostAddress() {
+        return this.socket.getHostAddress();
+    }
+
+    @Override
+    public int getPort() {
+        return this.socket.getPort();
     }
 
 }
