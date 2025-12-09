9e36681fa8344ac42b11f7270d407008ae43a3a9
[jesus romero] <jesus.romerovidal@gmail.com>
2025-12-09 19:30:08 +0100

    feat: add open use case to graphics #8

--- connect4/src/connect4/views/graphics/gameLoop/start/StartPanelView.java
+++ connect4/src/connect4/views/graphics/gameLoop/start/StartPanelView.java
@@ -4,53 +4,92 @@ import connect4.controllers.core.StartController;
 import connect4.types.PlayerType;
 import connect4.views.graphics.gameLoop.GameLoopView;
 
+import java.awt.Component;
 import java.awt.FlowLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.concurrent.CountDownLatch;
 
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
-public class StartPanelView extends GameLoopView implements ActionListener {
+public class StartPanelView extends GameLoopView {
 
     private StartController startController;
-    private final JComboBox<PlayerType>[] cBoxPlayers;
-    protected JButton button;
     private CountDownLatch latch;
 
     public StartPanelView(StartController startController, CountDownLatch latch) {
         assert (startController != null);
         this.startController = startController;
         this.latch = latch;
-        this.cBoxPlayers = new JComboBox[this.startController.getNumberPlayers()];
     }
 
     public void write() {
-        this.setLayout(new FlowLayout(FlowLayout.CENTER));
-        this.add(new JLabel("SELECT PLAYERS:"));
-        for (int i = 0; i < this.startController.getNumberPlayers(); i++) {
-            this.cBoxPlayers[i] = new JComboBox<>(this.startController.getPlayerTypes());
-            this.add(this.cBoxPlayers[i]);
-        }
-        this.button = new JButton("Play");
-        this.add(button);
-        this.button.addActionListener(this);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        this.setAlignmentX(Component.CENTER_ALIGNMENT);
+        this.writeNewGameMenu();
+        this.writeOpenGameMenu();
     }
 
-    public void addPlayers() {
-        for (JComboBox<PlayerType> jComboBox : cBoxPlayers) {
-            PlayerType playerType = (PlayerType) jComboBox.getSelectedItem();
-            this.startController.addPlayer(playerType);
+    private void writeNewGameMenu() {
+
+        class NewGameMenu {
+            JPanel newGamePanel;
+            JButton button;
+            private final JComboBox<PlayerType>[] cBoxPlayers;
+
+            public NewGameMenu() {
+                this.newGamePanel = new JPanel();
+                this.newGamePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+                this.newGamePanel.add(new JLabel("SELECT PLAYERS:"));
+                this.cBoxPlayers = new JComboBox[StartPanelView.this.startController.getNumberPlayers()];
+                for (int i = 0; i < StartPanelView.this.startController.getNumberPlayers(); i++) {
+                    this.cBoxPlayers[i] = new JComboBox<>(StartPanelView.this.startController.getPlayerTypes());
+                    newGamePanel.add(this.cBoxPlayers[i]);
+                }
+                this.button = new JButton("Play");
+                button.addActionListener(e -> {
+                    this.addPlayers();
+                    StartPanelView.this.startController.nextState();
+                    StartPanelView.this.latch.countDown();
+                });
+                newGamePanel.add(button);
+                StartPanelView.this.add(newGamePanel);
+            }
+
+            private void addPlayers() {
+                for (JComboBox<PlayerType> jComboBox : cBoxPlayers) {
+                    PlayerType playerType = (PlayerType) jComboBox.getSelectedItem();
+                    StartPanelView.this.startController.addPlayer(playerType);
+                }
+            }
+
+            public JPanel getNewGamePanel() {
+                return this.newGamePanel;
+            }
         }
+
+        this.add(new NewGameMenu().getNewGamePanel());
+
     }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        this.addPlayers();
-        this.startController.nextState();
-        this.latch.countDown();
+    private void writeOpenGameMenu() {
+        JPanel openGamePanel = new JPanel();
+        openGamePanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+        openGamePanel.add(new JLabel("OPEN SAVED GAME:"));
+        JComboBox<String> jComboBox = new JComboBox<>(this.startController.getGameNames());
+        openGamePanel.add(jComboBox);
+        JButton openGame = new JButton("Open");
+        openGame.addActionListener(e -> {
+            if (jComboBox.getSelectedItem() != null) {
+                String gameName = (String) jComboBox.getSelectedItem();
+                this.startController.start(gameName);
+                this.latch.countDown();
+            }
+        });
+        openGamePanel.add(openGame);
+        this.add(openGamePanel);
     }
 
 }
